6ae907adc5201ef2d0f34d5d4ceaaf7b02e21090
anasahmed01 <zainkhilji2323@gmail.com>
2023-03-09 01:34:08 +0500

    add_counter_view

---
+++ android/app/src/main/kotlin/com/example/stackedcappgg/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stackedcappgg
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/counter_view.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stackedcappgg/viewmodels/counter_viewmodel.dart';
+
+class CounterView extends StatelessWidget {
+  const CounterView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder.reactive(
+        viewModelBuilder: () => CounterViewModel(),
+        builder: (context, viewModel, child) {
+          return Scaffold(
+            body: Center(
+              child: SafeArea(
+                child: Column(
+                  children: [
+                    const Text('counter value'),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('Add Value'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('Navigate to home page'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        });
+  }
+}
